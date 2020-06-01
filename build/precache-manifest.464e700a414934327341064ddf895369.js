856b1cec97cb48b07dd0f440bf0264202a18358d
Yunuen Moncada <moncadayunuen@gmail.com>
2020-06-01 10:28:02 -0500

    App has built

---
+++ build/precache-manifest.464e700a414934327341064ddf895369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb902c8c0c284a5e75413390e3fb57b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a06f898320d42b7f7fa",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "df6fc3c6a7e3fd1a81f7",
+    "url": "/static/css/main.dcc7720e.chunk.css"
+  },
+  {
+    "revision": "8a06f898320d42b7f7fa",
+    "url": "/static/js/2.aa905902.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.aa905902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6fc3c6a7e3fd1a81f7",
+    "url": "/static/js/main.bdb7323d.chunk.js"
+  },
+  {
+    "revision": "a60f7bad7681eff02d97",
+    "url": "/static/js/runtime-main.7b81ef09.js"
+  }
+]);
